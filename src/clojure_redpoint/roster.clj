8b4967b94f70f20ccea139fd79d54744bc952411
Eric Tobin <eatobin@gmail.com>
2015-09-07 22:22:49 -0500

    progress

--- src/clojure_redpoint/roster.clj
+++ src/clojure_redpoint/roster.clj
@@ -7,3 +7,11 @@
          {:name    "Karen"
           :history [[:Sally :Maggie] [:puff :fluffy]]}}
         [:kareny :history 1 0])
+
+(assoc-in {:ericky
+           {:name    "Eric"
+            :history [[:scott :brenda] [:spot :chula]]}
+           :kareny
+           {:name    "Karen"
+            :history [[:Sally :Maggie] [:puff :fluffy]]}}
+          [:kareny :history 2] [:neru :jacket])
