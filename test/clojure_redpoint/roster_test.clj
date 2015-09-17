802aec1286f3deb543d0dbd8bcd5d0dbf941b292
Eric Tobin <eatobin@gmail.com>
2015-09-17 09:27:50 -0500

    test progress

---
+++ test/clojure_redpoint/roster_test.clj
@@ -0,0 +1,55 @@
+(ns clojure-redpoint.roster-test
+  (:require [clojure.test :refer :all]
+            [clojure-redpoint.roster :refer :all]
+            [clojure-csv.core :as csv]))
+
+(defn setup []
+  (spit "roster-test.txt" "Blackhawks, 2010
+TroBro, Troy Brouwer, DavBol, JoeQue
+JoeQue, Joel Quenneville, TroBro, AndLad
+AdaBur, Adam Burish, DunKei, JonToe\n"))
+
+;(reset! people (into [] (yaml/parse-string
+;                          (slurp "people-test.yaml")))))
+;
+;(defn teardown [])
+;
+;(defn each-fixture [f]
+;  (setup)
+;  (f)
+;  (teardown))
+;
+;(use-fixtures :each each-fixture)
+;
+;(deftest add-person-test
+;  (is (= [{:name "Person One", :max-books 2}
+;          {:name "Person Two", :max-books 6}
+;          {:name "Fred" :max-books 3}]
+;         (add-person "Fred"))))
+;
+;(deftest add-person-2-test
+;  (is (= [{:name "Person One", :max-books 2}
+;          {:name "Person Two", :max-books 6}
+;          {:name "Wilma" :max-books 5}]
+;         (add-person "Wilma" 5))))
+;
+;(deftest get-person-test
+;  (add-person "Fred")
+;  (is (= {:max-books 3 :name "Fred"}
+;         (get-person "Fred"))))
+;
+;(deftest get-person-2-test
+;  (is (= nil
+;         (get-person "Fred"))))
+;
+;(deftest person-to-string-test
+;  (add-person "Fred Flintstone" 7)
+;  (is (= "Fred Flintstone (7 books)\n"
+;         (person-to-string "Fred Flintstone"))))
+;
+;(deftest write-people-test
+;  (reset! people [{:name "Fred" :max-books 3}])
+;  (write-people)
+;  (is (= [{:name "Fred" :max-books 3}]
+;         (into [] (yaml/parse-string
+;                    (slurp "people.yaml"))))))
