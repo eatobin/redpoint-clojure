22bf85678f99ad26735c2e1d6d8a084a368a7b5f
Eric Tobin <eatobin@gmail.com>
2015-10-04 14:48:40 -0500

    progress

--- src/clojure_redpoint/hat.clj
+++ src/clojure_redpoint/hat.clj
@@ -1,24 +1,25 @@
 (ns clojure-redpoint.hat
   (:require [clojure-redpoint.roster :refer :all]))
-  
+
 (def pucks (atom []))
-(def discards (atom [])
-  
+
+(def discards (atom []))
+
 (defn make-hat [r]
-  (reset! pucks (keys (deref r)))
+  (reset! pucks (into [] (keys (deref r))))
   (reset! discards []))
-  
-(defn draw-puck
+
+(defn draw-puck []
   (rand-nth (deref pucks)))
-  
+
 (defn remove-puck [p]
-  (swap! r dissoc (deref pucks) p))
-  
-(defn discard-puck [p]
-  (if (dissoc (deref pucks) p)
-         )
-  
-  def discard_puck(player_code)
-    if @pucks.delete(player_code)
-      @discards.push(player_code)
-    end
+  (reset! pucks (into [] (remove #{p} (deref pucks)))))
+
+;(defn discard-puck [p]
+;  (if (dissoc (deref pucks) p)
+;         )
+
+;def discard_puck(player_code)
+;  if @pucks.delete(player_code)
+;    @discards.push(player_code)
+;  end
