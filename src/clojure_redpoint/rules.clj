8bf593e6f9a5bc31b3440035be3a31822417885e
Eric Tobin <eatobin@gmail.com>
2015-10-06 23:18:19 -0500

    rules work!!!!

--- src/clojure_redpoint/rules.clj
+++ src/clojure_redpoint/rules.clj
@@ -10,8 +10,8 @@
     (not= gr ge-giving-to)))
 
 (defn givee-not-repeat [gr ge y]
-  (let [past4 (range (- y 1) (- y 4) -1)
-        past (filter #(>= % 0) past4)
+  (let [past (filter #(>= % 0)
+                     (range (- y 1) (- y 4) -1))
         gr-y (partial get-givee-code gr)
         ge-in-yrs (into [] (map gr-y past))]
     (not-any? #{ge} ge-in-yrs)))
