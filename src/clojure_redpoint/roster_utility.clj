68e6cbb1009cf6c56ade991e91c9ed25adc727ab
Eric Tobin <eatobin@gmail.com>
2016-08-31 15:53:10 -0500

    prgress

--- src/clojure_redpoint/roster_utility.clj
+++ src/clojure_redpoint/roster_utility.clj
@@ -2,6 +2,14 @@
   (:require [clojure-csv.core :as csv]
             [clojure.string :as cs]))
 
-(defn make-roster-list [roster-string]
+(defn make-roster-list
+  "Returns a lazy roster-list"
+  [roster-string]
   (let [de-spaced (cs/replace roster-string #", " ",")]
     (csv/parse-csv de-spaced)))
+
+(defn make-roster-info [roster-list]
+  (first roster-list))
+
+(defn make-players-list [roster-list]
+  (rest roster-list))
