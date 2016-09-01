5981b7485096332526e433c5cbfc0d54a5e27161
Eric Tobin <eatobin@gmail.com>
2016-09-01 10:00:01 -0500

    roster-utility progress

--- src/clojure_redpoint/roster_utility.clj
+++ src/clojure_redpoint/roster_utility.clj
@@ -13,3 +13,12 @@
 
 (defn make-players-list [roster-list]
   (rest roster-list))
+
+(defn make-gift-pair [givee giver]
+  (hash-map
+    :givee givee
+    :giver giver))
+
+(defn make-player [p-name gift-history]
+  (hash-map :name p-name
+            :gift-history gift-history))
