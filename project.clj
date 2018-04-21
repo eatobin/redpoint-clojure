5997f90aea2a6d60d3a8f55484ab125c7317d662
Eric Tobin <eatobin@gmail.com>
2018-04-21 10:10:59 -0500

    clojure 1.9.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [clojure-csv/clojure-csv "2.0.1"]]
   :main ^:skip-aot clojure-redpoint.main
   :target-path "target/%s"
