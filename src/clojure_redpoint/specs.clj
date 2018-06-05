15cabd7112cde4b95791f13d243a1a09c07de419
Eric Tobin <eatobin@gmail.com>
2018-06-05 16:27:44 -0500

    roster progress

--- src/clojure_redpoint/specs.clj
+++ src/clojure_redpoint/specs.clj
@@ -1,9 +1,21 @@
 (ns clojure-redpoint.specs
-  (:require [clojure-redpoint.roster-utility :as ru]
+  (:require [clojure-redpoint.roster :as rost]
             [clojure.spec.alpha :as s]
             [clojure.spec.test.alpha :as stest]
             [clojure.spec.gen.alpha :as gen]))
 
 (gen/generate (s/gen int?))
 
-(stest/check `ru/make-roster-seq)
+(in-ns 'clojure-redpoint.roster)
+(def rs "The Beatles, 2014\nRinSta, Ringo Starr, JohLen, GeoHar\nJohLen, John Lennon, PauMcc, RinSta\nGeoHar, George Harrison, RinSta, PauMcc\nPauMcc, Paul McCartney, GeoHar, JohLen\n")
+
+(stest/check `extract-roster-info)
+(stest/check `extract-players-list)
+(s/conform vector?
+           (extract-roster-info rs))
+(s/conform nil?
+           (extract-roster-info ""))
+(s/conform ::plrs-list
+           (extract-players-list rs))
+(s/conform ::plrs-list
+           (extract-players-list ""))
