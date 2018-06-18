f931a043a2ce3bab0665f5e83684f09a479c97b5
Eric Tobin <eatobin@gmail.com>
2018-06-17 20:23:55 -0500

    works

--- src/clojure_redpoint/roster.clj
+++ src/clojure_redpoint/roster.clj
@@ -118,18 +118,18 @@
         :args (s/cat :g-pair :unq/gift-pair)
         :ret ::giver)
 
-;(defn set-gift-pair-in-gift-history [g-year g-pair g-hist]
-;  (assoc g-hist g-year g-pair))
 (defn set-gift-pair-in-gift-history [g-hist g-year g-pair]
   (assoc g-hist g-year g-pair))
-;(s/fdef set-gift-pair-in-gift-history
-;        :args (s/cat :g-year (s/and int? #(< % (count :g-hist))) :g-pair :unq/gift-pair :g-hist :unq/gift-history)
-;        :ret :unq/gift-history)
 (s/fdef set-gift-pair-in-gift-history
-        :args (s/cat :g-hist :unq/gift-history
-                     :g-year #(< % (count :g-hist))
-                     :g-pair :unq/gift-pair)
+        :args (s/and (s/cat :g-hist :unq/gift-history
+                            :g-year int?
+                            :g-pair :unq/gift-pair)
+                     #(< (:g-year %) (count (:g-hist %)))
+                     #(> (:g-year %) -1))
         :ret :unq/gift-history)
+
+
+
 
 ;(s/fdef set-gift-pair-in-gift-history
 ;        :args (s/cat :g-hist :unq/gift-history
