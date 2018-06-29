868ea298d69ab1fd468faa36f837e75429e6b2eb
Eric Tobin <eatobin@gmail.com>
2018-06-29 15:11:41 -0500

    add

--- src/clojure_redpoint/specs.clj
+++ src/clojure_redpoint/specs.clj
@@ -10,3 +10,4 @@
 (stest/check `rost/get-gift-history-in-player)
 (stest/check `rost/get-gift-pair-in-gift-history)
 (stest/check `rost/get-givee-in-gift-pair)
+(stest/check `rost/set-gift-pair-in-gift-history)
