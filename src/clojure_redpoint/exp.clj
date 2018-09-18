7759af36f83414c2a6c9715dbd4bec2b88f941c8
Eric Tobin <eatobin@gmail.com>
2018-09-18 14:39:24 -0500

    all working!

--- src/clojure_redpoint/exp.clj
+++ src/clojure_redpoint/exp.clj
@@ -2,83 +2,38 @@
   (:require [clojure.string :as str]))
 
 (def roster-string-ok "The Beatles, 2014\nRinSta, Ringo Starr, JohLen, GeoHar\nJohLen, John Lennon, PauMcc, RinSta\nGeoHar, George Harrison, RinSta, PauMcc\nPauMcc, Paul McCartney, GeoHar, JohLen\n")
-
-(def roster-string-bad-length "The Beatles, 2014\nRinSta, Ringo Starr, JohLen, GeoHar\nJohLen, John Lennon, PauMcc, RinSta\nGeoHar, George Harrison, RinSta, PauMcc")
-
+(def roster-string-bad-length "The Beatles, 2014\nRinSta, Ringo Starr, JohLen, GeoHar\nJohLen, John Lennon, PauMcc, RinSta\nGeoHar, George Harrison, RinSta, PauMcc\n")
 (def roster-string-bad-info1 "The Beatles\nRinSta, Ringo Starr, JohLen, GeoHar\nJohLen, John Lennon, PauMcc, RinSta\nGeoHar, George Harrison, RinSta, PauMcc\nPauMcc, Paul McCartney, GeoHar, JohLen")
-
 (def roster-string-bad-info2 ",2014\nRinSta, Ringo Starr, JohLen, GeoHar\nJohLen, John Lennon, PauMcc, RinSta\nGeoHar, George Harrison, RinSta, PauMcc\nPauMcc, Paul McCartney, GeoHar, JohLen")
-
 (def roster-string-bad-info3 "The Beatles,2096\nRinSta, Ringo Starr, JohLen, GeoHar\nJohLen, John Lennon, PauMcc, RinSta\nGeoHar, George Harrison, RinSta, PauMcc\nPauMcc, Paul McCartney, GeoHar, JohLen")
-
 (def roster-string-bad-info4 "The Beatles, 1896\nRinSta, Ringo Starr, JohLen, GeoHar\nJohLen, John Lennon, PauMcc, RinSta\nGeoHar, George Harrison, RinSta, PauMcc\nPauMcc, Paul McCartney, GeoHar, JohLen")
-
 (def roster-string-bad-info5 "The Beatles, Pony\nRinSta, Ringo Starr, JohLen, GeoHar\nJohLen, John Lennon, PauMcc, RinSta\nGeoHar, George Harrison, RinSta, PauMcc\nPauMcc, Paul McCartney, GeoHar, JohLen")
 
-;(defn de-space [rs] (str/replace rs #", " ","))
-;
-;(def lines (str/split-lines (de-space roster-string)))
-;
-;(get lines 0)
-;
-;(def info (get lines 0))
-;
-;(def info-bad "The Beatles,")
-;
-;(str/split info #",")
-;
-;(str/split info-bad #",")
-;
-;(def info-bad2 ",2014")
-;
-;(str/split info-bad2 #",")
-;
-;(def info-bad3 "The Beatles,2096")
-;
-;(def info-bad4 "The Beatles,1896")
-;
-;(def info-bad5 "The Beatles,Pony")
-;
-;(str/split "" #",")
-;
-;(str/split " " #",")
-;
-;(def freqs (frequencies roster-string))
-;
-;(count (filter #(= % \newline) roster-string))
-;
-;(str/blank? roster-string)
-;
-;(= 2 (count (str/split info #",")))
-;
-;(get info 1)
-;
-;(def year (read-string (get (str/split info #",") 1)))
-;
-;(#(<= 1956 % 2056) year)
-;
-;(<= 5 (count (filter #(= % \newline) roster-string)))
+;(-> (/ 144 12) (/,,, 2 3) str keyword (list,,, :33) (count) (= 2))
 
-(defn string-valid?
+(defn not-blank-string?
   "Return true if string is not nil, empty or only spaces"
   [string]
   (not (str/blank? string)))
 
+(defn scrub
+  "Remove the spaces between CSVs and any final \n"
+  [roster-string]
+  (->
+    roster-string
+    (str/replace #", " ",")
+    (str/trim)))
+
 (defn roster-string-valid?
   "A valid string of <= 4 newlines?"
   [roster-string]
-  (and (string-valid? roster-string)
-       (<= 4 (count (filter #(= % \newline) roster-string)))))
-
-(defn de-space
-  "Remove the spaces between CSVs"
-  [roster-string]
-  (str/replace roster-string #", " ","))
+  (and (not-blank-string? roster-string)
+       (<= 4 (count (filter #(= % \newline) (scrub roster-string))))))
 
 (defn lines
   "Split string into lines"
   [roster-string]
-  (str/split-lines (de-space roster-string)))
+  (str/split-lines (scrub roster-string)))
 
 (defn make-info-string
   "Return a string of first line if valid string parameter"
@@ -86,17 +41,15 @@
   (if (roster-string-valid? roster-string)
     (->
       roster-string
-      de-space
+      scrub
       lines
       (get 0))
     nil))
 
-;(def info (make-info-string roster-string))
-
 (defn info-string-valid?
   "Return true if info-string not blank, name not blank and 1956 <= year <= 2056"
   [info-string]
-  (and (string-valid? info-string)
+  (and (not-blank-string? info-string)
        (let [info-line (->
                          info-string
                          (str/split #","))]
@@ -108,7 +61,7 @@
            (->
              info-line
              (get 0)
-             (string-valid?))
+             (not-blank-string?))
            (->
              info-line
              (get 1)
@@ -121,5 +74,10 @@
              (Integer/parseInt)
              (#(<= 1956 % 2056)))))))
 
-(-> (/ 144 12) (/,,, 2 3) str keyword (list,,, :33) (count) (= 2))
-(str/trim roster-string-ok)
+(info-string-valid? (make-info-string roster-string-ok))
+(info-string-valid? (make-info-string roster-string-bad-length))
+(info-string-valid? (make-info-string roster-string-bad-info1))
+(info-string-valid? (make-info-string roster-string-bad-info2))
+(info-string-valid? (make-info-string roster-string-bad-info3))
+(info-string-valid? (make-info-string roster-string-bad-info4))
+(info-string-valid? (make-info-string roster-string-bad-info5))
