002531d20537897e11bee169adccb90e8f7815dd
Eric Tobin <eatobin@gmail.com>
2018-10-10 16:09:00 -0500

    hats progress

--- src/clojure_redpoint/hats.clj
+++ src/clojure_redpoint/hats.clj
@@ -1,20 +1,22 @@
 (ns clojure-redpoint.hats
-  (:require [clojure-redpoint.roster :refer :all]))
+  (:require [clojure.spec.alpha :as s]
+            [orchestra.spec.test :as st]
+            [clojure.repl :refer :all]))
 
 (defn make-hat [plrs-map]
   (into [] (keys plrs-map)))
 
-(defn remove-puck-givee [ge-hat givee]
-  (into [] (filter #(not= % givee) ge-hat)))
-
-(defn remove-puck-giver [gr-hat giver]
-  (into [] (filter #(not= % giver) gr-hat)))
-
-(defn discard-puck-givee [discards givee]
-  (conj discards givee))
-
-(defn return-discards [ge-hat discards]
-  (into ge-hat discards))
-
-(defn empty-discards [_]
-  [])
+;(defn remove-puck-givee [ge-hat givee]
+;  (into [] (filter #(not= % givee) ge-hat)))
+;
+;(defn remove-puck-giver [gr-hat giver]
+;  (into [] (filter #(not= % giver) gr-hat)))
+;
+;(defn discard-puck-givee [discards givee]
+;  (conj discards givee))
+;
+;(defn return-discards [ge-hat discards]
+;  (into ge-hat discards))
+;
+;(defn empty-discards [_]
+;  [])
--- test/clojure_redpoint/hats_test.clj
+++ test/clojure_redpoint/hats_test.clj
@@ -1,26 +1,31 @@
 (ns clojure-redpoint.hats-test
   (:require [clojure.test :refer :all]
-            [clojure-redpoint.hats :refer :all]
-            [clojure-redpoint.roster-test :refer :all]))
+            [clojure-redpoint.hats :refer :all]))
 
-(def test-hat [:RinSta :JohLen :GeoHar :PauMcc])
+(def test-hat [:PauMcc :GeoHar :JohLen :RinSta])
+(def players-map {:PauMcc {:name         "Paul McCartney",
+                           :gift-history [{:giver :JohLen, :givee :GeoHar}]},
+                  :GeoHar {:name         "George Harrison",
+                           :gift-history [{:giver :PauMcc, :givee :RinSta}]},
+                  :JohLen {:name "John Lennon", :gift-history [{:giver :RinSta, :givee :PauMcc}]},
+                  :RinSta {:name "Ringo Starr", :gift-history [{:giver :GeoHar, :givee :JohLen}]}})
 
 (deftest make-hats-test
-  (is (= [:RinSta :JohLen :GeoHar :PauMcc]
+  (is (= test-hat
          (make-hat players-map))))
 
-(deftest remove-puck-givee-test
-  (is (= [:JohLen :GeoHar :PauMcc]
-         (remove-puck-givee test-hat :RinSta))))
-
-(deftest remove-puck-givee-empty-test
-  (is (= []
-         (remove-puck-givee [] :RinSta))))
-
-(deftest discard-puck-givee-test
-  (is (= [:PauMcc :JohLen]
-         (discard-puck-givee [:PauMcc] :JohLen))))
-
-(deftest return-discards-test
-  (is (= [:PauMcc :JohLen :GeoHar]
-         (return-discards [:PauMcc :JohLen] [:GeoHar]))))
+;(deftest remove-puck-givee-test
+;  (is (= [:JohLen :GeoHar :PauMcc]
+;         (remove-puck-givee test-hat :RinSta))))
+;
+;(deftest remove-puck-givee-empty-test
+;  (is (= []
+;         (remove-puck-givee [] :RinSta))))
+;
+;(deftest discard-puck-givee-test
+;  (is (= [:PauMcc :JohLen]
+;         (discard-puck-givee [:PauMcc] :JohLen))))
+;
+;(deftest return-discards-test
+;  (is (= [:PauMcc :JohLen :GeoHar]
+;         (return-discards [:PauMcc :JohLen] [:GeoHar]))))
