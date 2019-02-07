c34dc61f720656601a16df78ea8ad7a9c20d3912
Eric Tobin <eatobin@gmail.com>
2019-02-06 22:23:02 -0700

    1.10.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [clojure-csv/clojure-csv "2.0.2"]
                  [orchestra "2017.11.12-1"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}}
