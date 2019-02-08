8acf4c25b20ca9641e96ab4899e1538c84cc6166
Eric Tobin <eatobin@gmail.com>
2019-02-07 23:06:39 -0700

    new orchestra

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [clojure-csv/clojure-csv "2.0.2"]
-                 [orchestra "2017.11.12-1"]]
+                 [orchestra "2018.12.06-2"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}}
   :main clojure-redpoint.main)
