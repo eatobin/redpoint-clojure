0195b2680f81afb1bf87840044b2605c14146d73
Eric Tobin <eatobin@gmail.com>
2019-10-29 17:03:53 -0500

    player tests

--- test/clojure_redpoint/player_test.clj
+++ test/clojure_redpoint/player_test.clj
@@ -6,15 +6,22 @@
 (def player {:player-name  "Paul McCartney",
              :gift-history [{:giver :JohLen, :givee :GeoHar}]})
 
-(deftest get-player-name-in-player-test
+(deftest get-player-name-test
   (is (= "Paul McCartney"
          (plr/get-player-name player))))
 (s/conform ::plr/player-name
            (plr/get-player-name player))
 
-(deftest get-gift-history-in-player-test
+(deftest get-gift-history-test
   (is (= [{:givee :GeoHar
            :giver :JohLen}]
          (plr/get-gift-history player))))
 (s/conform :unq/gift-history
            (plr/get-gift-history player))
+
+(deftest get-gift-history-test
+  (is (= {:player-name  "Paul McCartney",
+          :gift-history [{:givee :nope :giver :yup}]}
+         (plr/set-gift-history player [{:givee :nope :giver :yup}]))))
+(s/conform :unq/player
+           (plr/set-gift-history player [{:givee :nope :giver :yup}]))
