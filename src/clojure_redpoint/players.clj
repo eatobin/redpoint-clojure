161e10ca4422f1247f8bc0740566f7ce575bcc9d
Eric Tobin <eatobin@gmail.com>
2020-05-15 13:19:01 -0500

    progress

--- src/clojure_redpoint/players.clj
+++ src/clojure_redpoint/players.clj
@@ -1,83 +1,99 @@
 (ns clojure-redpoint.players
-  (:require [clojure-redpoint.gift-pair :as gp]
-            [clojure-redpoint.gift-history :as gh]
-            [clojure-redpoint.player :as plr]
-            [clojure.spec.alpha :as s]
+  (:require [clojure.spec.alpha :as s]
             [orchestra.spec.test :as ostest]))
 
-(s/def :unq/players (s/map-of ::gh/player-key :unq/player))
+(s/def ::givee keyword?)
+(s/def ::giver keyword?)
+(s/def :unq/gift-pair (s/keys :req-un [::givee ::giver]))
+(s/def :unq/gift-history (s/coll-of :unq/gift-pair :kind vector?))
+(s/def ::player-key keyword?)
+(s/def ::gift-year (s/and int? #(> % -1)))
+(s/def ::player-name string?)
+(s/def :unq/player (s/keys :req-un [::player-name :unq/gift-history]))
+(s/def :unq/players (s/map-of ::player-key :unq/player))
 
-(defn get-player
-  "Returns a player given players and a player-key"
+(defn get-player-name
   [players plr-key]
-  (players plr-key))
-(s/fdef get-player
+  (get-in players [plr-key :player-name]))
+(s/fdef get-player-name
         :args (s/cat :players :unq/players
-                     :plr-key ::gh/player-key)
-        :ret (s/or :found :unq/player
+                     :plr-key ::player-key)
+        :ret (s/or :found ::player-name
                    :not-found nil?))
 
-(defn set-player
-  "Sets a player given players, a player-key and a new player"
-  [players plr-key player]
-  (assoc players plr-key player))
-(s/fdef set-player
-        :args (s/cat :players :unq/players
-                     :plr-key ::gh/player-key
-                     :player :unq/player)
-        :ret :unq/players)
-
-(defn add-year-players
-  "Add a year for each player in roster"
-  [players]
-  (into {}
-        (for [[plr-key player] players]
-          [plr-key (plr/add-year-player player plr-key)])))
-(s/fdef add-year-players
-        :args (s/cat :players :unq/players)
-        :ret :unq/players)
-
-(defn get-player-name-players
-  [players plr-key]
-  (->
-    players
-    (get-player plr-key)
-    :player-name))
-(s/fdef get-player-name-players
-        :args (s/cat :players :unq/players
-                     :plr-key ::gh/player-key)
-        :ret ::plr/player-name)
-
-(defn get-giv-ee-er-players
-  [players plr-key ee-er g-year]
-  (let [plr (get-player players plr-key)
-        gh (plr :gift-history)
-        gp (gh g-year)]
-    (if (= ee-er :ee)
-      (gp :givee)
-      (gp :giver))))
-(s/fdef get-giv-ee-er-players
-        :args (s/cat :players :unq/players
-                     :plr-key ::gh/player-key
-                     :ee-er ::gp/ee-er
-                     :g-year ::gh/gift-year)
-        :ret ::gp/giv)
-
-(defn set-giv-ee-er-players
-  [players plr-key g-year giv ee-er]
-  (let [plr (get-player players plr-key)
-        gh (plr :gift-history)
-        gp (gh g-year)
-        ngp (gp/set-giv-ee-er gp giv ee-er)
-        ngh (gh/set-gift-pair gh g-year ngp)
-        nplr (plr/set-gift-history plr ngh)]
-    (set-player players plr-key nplr)))
-(s/fdef set-giv-ee-er-players
-        :args (s/cat :players :unq/players
-                     :plr-key ::gh/player-key
-                     :g-year ::gh/gift-year
-                     :giv ::gp/giv
-                     :ee-er ::gp/ee-er)
-        :ret :unq/players)
+;(defn add-year
+;  "Adds a new placeholder year to the end of a gift history"
+;  [g-hist plr-key]
+;  (conj g-hist {:givee plr-key, :giver plr-key}))
+;(s/fdef add-year
+;        :args (s/cat :g-hist :unq/gift-history
+;                     :plr-key ::player-key)
+;        :ret :unq/gift-history)
+;
+;
+;
+;(defn get-player
+;  "Returns a player given players and a player-key"
+;  [players plr-key]
+;  (players plr-key))
+;(s/fdef get-player
+;        :args (s/cat :players :unq/players
+;                     :plr-key ::gh/player-key)
+;        :ret (s/or :found :unq/player
+;                   :not-found nil?))
+;
+;(defn set-player
+;  "Sets a player given players, a player-key and a new player"
+;  [players plr-key player]
+;  (assoc players plr-key player))
+;(s/fdef set-player
+;        :args (s/cat :players :unq/players
+;                     :plr-key ::gh/player-key
+;                     :player :unq/player)
+;        :ret :unq/players)
+;
+;(defn add-year-players
+;  "Add a year for each player in roster"
+;  [players]
+;  (into {}
+;        (for [[plr-key player] players]
+;          [plr-key (plr/add-year-player player plr-key)])))
+;(s/fdef add-year-players
+;        :args (s/cat :players :unq/players)
+;        :ret :unq/players)
+;
+;
+;
+;(defn get-giv-ee-er-players
+;  [players plr-key ee-er g-year]
+;  (let [plr (get-player players plr-key)
+;        gh (plr :gift-history)
+;        gp (gh g-year)]
+;    (if (= ee-er :ee)
+;      (gp :givee)
+;      (gp :giver))))
+;(s/fdef get-giv-ee-er-players
+;        :args (s/cat :players :unq/players
+;                     :plr-key ::gh/player-key
+;                     :ee-er ::gp/ee-er
+;                     :g-year ::gh/gift-year)
+;        :ret ::gp/giv)
+;
+;(defn set-giv-ee-er-players
+;  [players plr-key g-year giv ee-er]
+;  (let [plr (get-player players plr-key)
+;        gh (plr :gift-history)
+;        gp (gh g-year)
+;        ngp (gp/set-giv-ee-er gp giv ee-er)
+;        ngh (gh/set-gift-pair gh g-year ngp)
+;        nplr (plr/set-gift-history plr ngh)]
+;    (set-player players plr-key nplr)))
+;(s/fdef set-giv-ee-er-players
+;        :args (s/cat :players :unq/players
+;                     :plr-key ::gh/player-key
+;                     :g-year ::gh/gift-year
+;                     :giv ::gp/giv
+;                     :ee-er ::gp/ee-er)
+;        :ret :unq/players)
 
 (ostest/instrument)
--- test/clojure_redpoint/players_test.clj
+++ test/clojure_redpoint/players_test.clj
@@ -1,7 +1,5 @@
 (ns clojure-redpoint.players-test
   (:require [clojure.test :refer :all]
-            [clojure-redpoint.gift-pair :as gp]
-            [clojure-redpoint.player :as plr]
             [clojure-redpoint.players :as plrs]
             [clojure.spec.alpha :as s]))
 
@@ -12,81 +10,92 @@
               :JohLen {:player-name "John Lennon", :gift-history [{:giver :RinSta, :givee :PauMcc}]},
               :RinSta {:player-name "Ringo Starr", :gift-history [{:giver :GeoHar, :givee :JohLen}]}})
 
-(def new-bee-players {:PauMcc {:player-name  "Paul McCartney",
-                               :gift-history [{:giver :JohLen, :givee :GeoHar}]},
-                      :GeoHar {:player-name  "George Harrison",
-                               :gift-history [{:giver :PauMcc, :givee :RinSta}]},
-                      :JohLen {:player-name "John Lennon", :gift-history [{:giver :RinSta, :givee :PauMcc}]},
-                      :RinSta {:player-name "New Bee", :gift-history [{:giver :NewBee, :givee :NewBee}]}})
-
-(def extended-players {:PauMcc {:player-name  "Paul McCartney",
-                                :gift-history [{:giver :JohLen, :givee :GeoHar} {:givee :PauMcc, :giver :PauMcc}]},
-                       :GeoHar {:player-name  "George Harrison",
-                                :gift-history [{:giver :PauMcc, :givee :RinSta} {:givee :GeoHar, :giver :GeoHar}]},
-                       :JohLen {:player-name "John Lennon", :gift-history [{:giver :RinSta, :givee :PauMcc} {:givee :JohLen, :giver :JohLen}]},
-                       :RinSta {:player-name "Ringo Starr", :gift-history [{:giver :GeoHar, :givee :JohLen} {:givee :RinSta, :giver :RinSta}]}})
+;(def new-bee-players {:PauMcc {:player-name  "Paul McCartney",
+;                               :gift-history [{:giver :JohLen, :givee :GeoHar}]},
+;                      :GeoHar {:player-name  "George Harrison",
+;                               :gift-history [{:giver :PauMcc, :givee :RinSta}]},
+;                      :JohLen {:player-name "John Lennon", :gift-history [{:giver :RinSta, :givee :PauMcc}]},
+;                      :RinSta {:player-name "New Bee", :gift-history [{:giver :NewBee, :givee :NewBee}]}})
+;
+;(def extended-players {:PauMcc {:player-name  "Paul McCartney",
+;                                :gift-history [{:giver :JohLen, :givee :GeoHar} {:givee :PauMcc, :giver :PauMcc}]},
+;                       :GeoHar {:player-name  "George Harrison",
+;                                :gift-history [{:giver :PauMcc, :givee :RinSta} {:givee :GeoHar, :giver :GeoHar}]},
+;                       :JohLen {:player-name "John Lennon", :gift-history [{:giver :RinSta, :givee :PauMcc} {:givee :JohLen, :giver :JohLen}]},
+;                       :RinSta {:player-name "Ringo Starr", :gift-history [{:giver :GeoHar, :givee :JohLen} {:givee :RinSta, :giver :RinSta}]}})
 
 (s/conform :unq/players
            players)
 
-(deftest get-player-pass-test
-  (is (= {:player-name "John Lennon", :gift-history [{:giver :RinSta, :givee :PauMcc}]}
-         (plrs/get-player players :JohLen))))
-(s/conform (s/or :found :unq/player
-                 :not-found nil?)
-           (plrs/get-player players :JohLen))
-
-(deftest get-player-fail-test
-  (is (= nil
-         (plrs/get-player players :JohLenX))))
-(s/conform (s/or :found :unq/player
-                 :not-found nil?)
-           (plrs/get-player players :JohLenX))
-
-(deftest set-player-test
-  (is (= new-bee-players
-         (plrs/set-player
-           players
-           :RinSta {:player-name "New Bee", :gift-history [{:giver :NewBee, :givee :NewBee}]}))))
-(s/conform :unq/players
-           (plrs/set-player
-             players
-             :RinSta {:player-name "New Bee", :gift-history [{:giver :NewBee, :givee :NewBee}]}))
-
-(deftest add-year-players-test
-  (is (= extended-players
-         (plrs/add-year-players players))))
-(s/conform :unq/players
-           (plrs/add-year-players players))
-
-(deftest get-player-name-players-test
+(deftest get-player-name-pass-test
   (is (= "George Harrison"
-         (plrs/get-player-name-players players :GeoHar))))
-(s/conform ::plr/player-name
-           (plrs/get-player-name-players players :GeoHar))
+         (plrs/get-player-name players :GeoHar))))
+(s/conform (s/or :found ::plrs/player-name
+                 :not-found nil?)
+           (plrs/get-player-name players :GeoHar))
 
-(deftest get-giv-ee-er-players-test
-  (is (= :RinSta
-         (plrs/get-giv-ee-er-players players :GeoHar :ee 0)))
-  (is (= :PauMcc
-         (plrs/get-giv-ee-er-players players :GeoHar :er 0))))
-(s/conform ::gp/giv
-           (plrs/get-giv-ee-er-players players :GeoHar :ee 0))
-(s/conform ::gp/giv
-           (plrs/get-giv-ee-er-players players :GeoHar :ee 0))
+(deftest get-player-name-fail-test
+  (is (nil?
+        (plrs/get-player-name players :GeoHarX))))
+(s/conform (s/or :found ::plrs/player-name
+                 :not-found nil?)
+           (plrs/get-player-name players :GeoHarX))
 
-(deftest set-giv-ee-er-players-test
-  (is (= {:PauMcc {:player-name "Paul McCartney", :gift-history [{:giver :JohLen, :givee :GeoHar}]},
-          :GeoHar {:player-name "George Harrison", :gift-history [{:giver :PauMcc, :givee :you}]},
-          :JohLen {:player-name "John Lennon", :gift-history [{:giver :RinSta, :givee :PauMcc}]},
-          :RinSta {:player-name "Ringo Starr", :gift-history [{:giver :GeoHar, :givee :JohLen}]}}
-         (plrs/set-giv-ee-er-players players :GeoHar 0 :you :ee)))
-  (is (= {:PauMcc {:player-name "Paul McCartney", :gift-history [{:giver :JohLen, :givee :GeoHar}]},
-          :GeoHar {:player-name "George Harrison", :gift-history [{:giver :you, :givee :RinSta}]},
-          :JohLen {:player-name "John Lennon", :gift-history [{:giver :RinSta, :givee :PauMcc}]},
-          :RinSta {:player-name "Ringo Starr", :gift-history [{:giver :GeoHar, :givee :JohLen}]}}
-         (plrs/set-giv-ee-er-players players :GeoHar 0 :you :er))))
-(s/conform :unq/players
-           (plrs/set-giv-ee-er-players players :GeoHar 0 :you :ee))
-(s/conform :unq/players
-           (plrs/set-giv-ee-er-players players :GeoHar 0 :you :er))
+
+;(deftest get-player-pass-test
+;  (is (= {:player-name "John Lennon", :gift-history [{:giver :RinSta, :givee :PauMcc}]}
+;         (plrs/get-player players :JohLen))))
+;(s/conform (s/or :found :unq/player
+;                 :not-found nil?)
+;           (plrs/get-player players :JohLen))
+;
+;(deftest get-player-fail-test
+;  (is (= nil
+;         (plrs/get-player players :JohLenX))))
+;(s/conform (s/or :found :unq/player
+;                 :not-found nil?)
+;           (plrs/get-player players :JohLenX))
+;
+;(deftest set-player-test
+;  (is (= new-bee-players
+;         (plrs/set-player
+;           players
+;           :RinSta {:player-name "New Bee", :gift-history [{:giver :NewBee, :givee :NewBee}]}))))
+;(s/conform :unq/players
+;           (plrs/set-player
+;             players
+;             :RinSta {:player-name "New Bee", :gift-history [{:giver :NewBee, :givee :NewBee}]}))
+;
+;(deftest add-year-players-test
+;  (is (= extended-players
+;         (plrs/add-year-players players))))
+;(s/conform :unq/players
+;           (plrs/add-year-players players))
+;
+;
+;
+;(deftest get-giv-ee-er-players-test
+;  (is (= :RinSta
+;         (plrs/get-giv-ee-er-players players :GeoHar :ee 0)))
+;  (is (= :PauMcc
+;         (plrs/get-giv-ee-er-players players :GeoHar :er 0))))
+;(s/conform ::gp/giv
+;           (plrs/get-giv-ee-er-players players :GeoHar :ee 0))
+;(s/conform ::gp/giv
+;           (plrs/get-giv-ee-er-players players :GeoHar :ee 0))
+;
+;(deftest set-giv-ee-er-players-test
+;  (is (= {:PauMcc {:player-name "Paul McCartney", :gift-history [{:giver :JohLen, :givee :GeoHar}]},
+;          :GeoHar {:player-name "George Harrison", :gift-history [{:giver :PauMcc, :givee :you}]},
+;          :JohLen {:player-name "John Lennon", :gift-history [{:giver :RinSta, :givee :PauMcc}]},
+;          :RinSta {:player-name "Ringo Starr", :gift-history [{:giver :GeoHar, :givee :JohLen}]}}
+;         (plrs/set-giv-ee-er-players players :GeoHar 0 :you :ee)))
+;  (is (= {:PauMcc {:player-name "Paul McCartney", :gift-history [{:giver :JohLen, :givee :GeoHar}]},
+;          :GeoHar {:player-name "George Harrison", :gift-history [{:giver :you, :givee :RinSta}]},
+;          :JohLen {:player-name "John Lennon", :gift-history [{:giver :RinSta, :givee :PauMcc}]},
+;          :RinSta {:player-name "Ringo Starr", :gift-history [{:giver :GeoHar, :givee :JohLen}]}}
+;         (plrs/set-giv-ee-er-players players :GeoHar 0 :you :er))))
+;(s/conform :unq/players
+;           (plrs/set-giv-ee-er-players players :GeoHar 0 :you :ee))
+;(s/conform :unq/players
+;           (plrs/set-giv-ee-er-players players :GeoHar 0 :you :er))
