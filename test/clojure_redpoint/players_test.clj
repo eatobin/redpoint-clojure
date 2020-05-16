6660a1bf6eb3eff2b6f14e3eaf509852f40720af
Eric Tobin <eatobin@gmail.com>
2020-05-16 17:17:37 -0500

    give tests

--- test/clojure_redpoint/players_test.clj
+++ test/clojure_redpoint/players_test.clj
@@ -10,19 +10,19 @@
               :JohLen {:player-name "John Lennon", :gift-history [{:giver :RinSta, :givee :PauMcc}]},
               :RinSta {:player-name "Ringo Starr", :gift-history [{:giver :GeoHar, :givee :JohLen}]}})
 
-;(def new-bee-players {:PauMcc {:player-name  "Paul McCartney",
-;                               :gift-history [{:giver :JohLen, :givee :GeoHar}]},
-;                      :GeoHar {:player-name  "George Harrison",
-;                               :gift-history [{:giver :PauMcc, :givee :RinSta}]},
-;                      :JohLen {:player-name "John Lennon", :gift-history [{:giver :RinSta, :givee :PauMcc}]},
-;                      :RinSta {:player-name "New Bee", :gift-history [{:giver :NewBee, :givee :NewBee}]}})
-;
 (def extended-players {:PauMcc {:player-name  "Paul McCartney",
                                 :gift-history [{:giver :JohLen, :givee :GeoHar} {:givee :PauMcc, :giver :PauMcc}]},
                        :GeoHar {:player-name  "George Harrison",
                                 :gift-history [{:giver :PauMcc, :givee :RinSta} {:givee :GeoHar, :giver :GeoHar}]},
                        :JohLen {:player-name "John Lennon", :gift-history [{:giver :RinSta, :givee :PauMcc} {:givee :JohLen, :giver :JohLen}]},
                        :RinSta {:player-name "Ringo Starr", :gift-history [{:giver :GeoHar, :givee :JohLen} {:givee :RinSta, :giver :RinSta}]}})
+
+;(def new-bee-players {:PauMcc {:player-name  "Paul McCartney",
+;                               :gift-history [{:giver :JohLen, :givee :GeoHar}]},
+;                      :GeoHar {:player-name  "George Harrison",
+;                               :gift-history [{:giver :PauMcc, :givee :RinSta}]},
+;                      :JohLen {:player-name "John Lennon", :gift-history [{:giver :RinSta, :givee :PauMcc}]},
+;                      :RinSta {:player-name "New Bee", :gift-history [{:giver :NewBee, :givee :NewBee}]}})
 
 (s/conform :unq/players
            players)
@@ -47,13 +47,13 @@
 
 (deftest get-givee-giver-test
   (is (= :RinSta
-         (plrs/get-givee players :GeoHar 0))))
-(is (= :PauMcc
-        (plrs/get-giv-ee-er-players players :GeoHar :er 0))))
+         (plrs/get-givee players :GeoHar 0)))
+  (is (= :PauMcc
+         (plrs/get-giver players :GeoHar 0))))
 (s/conform ::plrs/givee
            (plrs/get-givee players :GeoHar 0))
-;#_(s/conform ::gp/giv
-;             (plrs/get-giv-ee-er-players players :GeoHar :ee 0))
+(s/conform ::plrs/giver
+           (plrs/get-giver players :GeoHar 0))
 
 ;(deftest get-player-pass-test
 ;  (is (= {:player-name "John Lennon", :gift-history [{:giver :RinSta, :givee :PauMcc}]}
