d992e58de60882f29eb81885caf019519caa52cf
Eric Tobin <eatobin@gmail.com>
2020-05-18 14:08:41 -0500

    progress!!

--- src/clojure_redpoint/hats.clj
+++ src/clojure_redpoint/hats.clj
@@ -7,37 +7,37 @@
 (s/def ::hat (s/coll-of ::gh/player-key :kind set?))
 (s/def ::discards (s/coll-of ::gh/player-key :kind set?))
 
-(defn make-hat [players]
-  (into #{} (keys players)))
+(defn make-hat [roster]
+  (into #{} (keys (roster :players))))
 (s/fdef make-hat
-        :args (s/cat :players :unq/players)
-        :ret ::hat)
+  :args (s/cat :roster :unq/roster)
+  :ret ::hat)
 
 (defn remove-puck [hat plr-key]
   (into #{} (filter #(not= % plr-key) hat)))
 (s/fdef remove-puck
-        :args (s/cat :hat ::hat
-                     :plr-key ::gh/player-key)
-        :ret ::hat)
+  :args (s/cat :hat ::hat
+               :plr-key ::gh/player-key)
+  :ret ::hat)
 
 (defn discard-givee [discards givee]
   (conj discards givee))
 (s/fdef discard-givee
-        :args (s/cat :discards ::discards :givee ::gp/givee)
-        :ret ::discards)
+  :args (s/cat :discards ::discards :givee ::gp/givee)
+  :ret ::discards)
 
 (defn return-discards [ge-hat discards]
   (into ge-hat discards))
 (s/fdef return-discards
-        :args (s/cat :ge-hat ::hat
-                     :discards ::discards)
-        :ret ::hat)
+  :args (s/cat :ge-hat ::hat
+               :discards ::discards)
+  :ret ::hat)
 
 (defn empty-discards
   []
   #{})
 (s/fdef empty-discards
-        :args (s/cat)
-        :ret ::discards)
+  :args (s/cat)
+  :ret ::discards)
 
 (ostest/instrument)
