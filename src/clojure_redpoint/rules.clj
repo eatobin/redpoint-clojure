8e3fb834968d1fa5d3b9370303e4f49f12afc3b0
Eric Tobin <eatobin@gmail.com>
2020-05-18 20:19:33 -0500

    rules_test.clj tests

--- src/clojure_redpoint/rules.clj
+++ src/clojure_redpoint/rules.clj
@@ -1,7 +1,5 @@
 (ns clojure-redpoint.rules
-  (:require [clojure-redpoint.gift-pair :as gp]
-            [clojure-redpoint.gift-history :as gh]
-            [clojure-redpoint.roster :as plrs]
+  (:require [clojure-redpoint.roster :as ros]
             [clojure.spec.alpha :as s]
             [orchestra.spec.test :as ostest]))
 
@@ -9,33 +7,33 @@
   "Test 1 - not giving to yourself"
   (not= self-key givee))
 (s/fdef givee-not-self?
-        :args (s/cat :self-key ::gh/player-key
-                     :givee ::gp/givee)
+        :args (s/cat :self-key ::ros/player-key
+                     :givee ::ros/givee)
         :ret boolean?)
 
-(defn givee-not-recip? [self-key givee g-year players]
+(defn givee-not-recip? [self-key givee g-year roster]
   "Test 2 - not giving to the person who is giving to you"
-  (let [recip (plrs/get-giv-ee-er-players players givee :ee g-year)]
+  (let [recip (ros/get-givee roster givee g-year)]
     (not= self-key recip)))
 (s/fdef givee-not-recip?
-        :args (s/cat :self-key ::gh/player-key
-                     :givee ::gp/givee
-                     :g-year ::gh/gift-year
-                     :players :unq/players)
+        :args (s/cat :self-key ::ros/player-key
+                     :givee ::ros/givee
+                     :g-year ::ros/gift-year
+                     :roster :unq/roster)
         :ret boolean?)
 
-(defn givee-not-repeat? [self-key givee g-year players]
+(defn givee-not-repeat? [self-key givee g-year roster]
   "Test 3 - not giving to someone you have given to in the past 3 years"
   (let [past (filter #(>= % 0)
                      (range (- g-year 1) (- g-year 4) -1))
-        ge-y (partial plrs/get-giv-ee-er-players players self-key :ee)
+        ge-y (partial ros/get-givee roster self-key)
         ge-in-yrs (into [] (map ge-y past))]
     (not-any? #{givee} ge-in-yrs)))
 (s/fdef givee-not-repeat?
-        :args (s/cat :self-key ::gh/player-key
-                     :givee ::gp/givee
-                     :g-year ::gh/gift-year
-                     :players :unq/players)
+        :args (s/cat :self-key ::ros/player-key
+                     :givee ::ros/givee
+                     :g-year ::ros/gift-year
+                     :roster :unq/roster)
         :ret boolean?)
 
 (ostest/instrument)
--- test/clojure_redpoint/rules_test.clj
+++ test/clojure_redpoint/rules_test.clj
@@ -1,29 +1,31 @@
 (ns clojure-redpoint.rules-test
   (:require [clojure.test :refer :all]
             [clojure-redpoint.rules :as rule]
-            [clojure-redpoint.roster :as plrs]
+            [clojure-redpoint.roster :as ros]
             [clojure.spec.alpha :as s]))
 
-(def beatles-plus-pm {:RinSta {:player-name "Ringo Starr", :gift-history [{:giver :KarLav, :givee :JohLen}]},
-                      :JohLen {:player-name "John Lennon", :gift-history [{:giver :RinSta, :givee :GeoHar}]},
-                      :GeoHar {:player-name  "George Harrison",
-                               :gift-history [{:giver :JohLen, :givee :PauMcc}]},
-                      :PauMcc {:player-name  "Paul McCartney",
-                               :gift-history [{:giver :GeoHar, :givee :EriTob}]},
-                      :EriTob {:player-name "Eric Tobin", :gift-history [{:giver :PauMcc, :givee :KarLav}]},
-                      :KarLav {:player-name  "Karen Lavengood",
-                               :gift-history [{:giver :EriTob, :givee :RinSta}]}})
+(def beatles-plus-pm {:roster-name "The Beatles",
+                      :roster-year 2014,
+                      :players     {:RinSta {:player-name "Ringo Starr", :gift-history [{:giver :KarLav, :givee :JohLen}]},
+                                    :JohLen {:player-name "John Lennon", :gift-history [{:giver :RinSta, :givee :GeoHar}]},
+                                    :GeoHar {:player-name  "George Harrison",
+                                             :gift-history [{:giver :JohLen, :givee :PauMcc}]},
+                                    :PauMcc {:player-name  "Paul McCartney",
+                                             :gift-history [{:giver :GeoHar, :givee :EriTob}]},
+                                    :EriTob {:player-name "Eric Tobin", :gift-history [{:giver :PauMcc, :givee :KarLav}]},
+                                    :KarLav {:player-name  "Karen Lavengood",
+                                             :gift-history [{:giver :EriTob, :givee :RinSta}]}}})
 
-(def extended ((comp plrs/add-year-players
-                     plrs/add-year-players
-                     plrs/add-year-players
-                     plrs/add-year-players)
+(def extended ((comp ros/add-year
+                     ros/add-year
+                     ros/add-year
+                     ros/add-year)
                beatles-plus-pm))
 
-(def beatles-plus-4 (plrs/set-giv-ee-er-players
-                      (plrs/set-giv-ee-er-players
-                        (plrs/set-giv-ee-er-players
-                          (plrs/set-giv-ee-er-players extended :RinSta 1 :GeoHar :ee) :RinSta 2 :PauMcc :ee) :RinSta 3 :EriTob :ee) :RinSta 4 :KarLav :ee))
+(def beatles-plus-4 (ros/set-givee
+                      (ros/set-givee
+                        (ros/set-givee
+                          (ros/set-givee extended :RinSta 1 :GeoHar) :RinSta 2 :PauMcc) :RinSta 3 :EriTob) :RinSta 4 :KarLav))
 
 (deftest givee-not-self-test
   (is (= true
