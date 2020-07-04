5a4565190fcab79b8a727e151f28c35c073738c3
Eric Tobin <eatobin@gmail.com>
2020-07-04 16:51:48 -0500

    rules broken

--- test/clojure_redpoint/rules_test.clj
+++ test/clojure_redpoint/rules_test.clj
@@ -4,23 +4,25 @@
             [clojure-redpoint.roster :as ros]
             [clojure.spec.alpha :as s]))
 
-(def beatles-plus-pm {:roster-name "The Beatles",
-                      :roster-year 2014,
-                      :players     {:RinSta {:player-name "Ringo Starr", :gift-history [{:giver :KarLav, :givee :JohLen}]},
-                                    :JohLen {:player-name "John Lennon", :gift-history [{:giver :RinSta, :givee :GeoHar}]},
-                                    :GeoHar {:player-name  "George Harrison",
-                                             :gift-history [{:giver :JohLen, :givee :PauMcc}]},
-                                    :PauMcc {:player-name  "Paul McCartney",
-                                             :gift-history [{:giver :GeoHar, :givee :EriTob}]},
-                                    :EriTob {:player-name "Eric Tobin", :gift-history [{:giver :PauMcc, :givee :KarLav}]},
-                                    :KarLav {:player-name  "Karen Lavengood",
-                                             :gift-history [{:giver :EriTob, :givee :RinSta}]}}})
+(def roster {:roster-name "The Beatles",
+             :roster-year 2014,
+             :players     {:RinSta {:player-name "Ringo Starr", :gift-history [{:giver :KarLav, :givee :JohLen}]},
+                           :JohLen {:player-name "John Lennon", :gift-history [{:giver :RinSta, :givee :GeoHar}]},
+                           :GeoHar {:player-name  "George Harrison",
+                                    :gift-history [{:giver :JohLen, :givee :PauMcc}]},
+                           :PauMcc {:player-name  "Paul McCartney",
+                                    :gift-history [{:giver :GeoHar, :givee :EriTob}]},
+                           :EriTob {:player-name "Eric Tobin", :gift-history [{:giver :PauMcc, :givee :KarLav}]},
+                           :KarLav {:player-name  "Karen Lavengood",
+                                    :gift-history [{:giver :EriTob, :givee :RinSta}]}}})
+
+(def players (roster :players))
 
 (def extended ((comp ros/add-year
                      ros/add-year
                      ros/add-year
                      ros/add-year)
-               beatles-plus-pm))
+               players))
 
 (def beatles-plus-4 (ros/update-givee
                       (ros/update-givee
@@ -37,11 +39,11 @@
 
 (deftest givee-not-recip-test
   (is (= true
-         (rule/givee-not-recip? :RinSta :JohLen 0 beatles-plus-pm)))
+         (rule/givee-not-recip? :RinSta :JohLen 0 roster)))
   (is (= false
-         (rule/givee-not-recip? :RinSta :KarLav 0 beatles-plus-pm))))
+         (rule/givee-not-recip? :RinSta :KarLav 0 roster))))
 (s/conform boolean?
-           (rule/givee-not-recip? :RinSta :JohLen 0 beatles-plus-pm))
+           (rule/givee-not-recip? :RinSta :JohLen 0 roster))
 
 (deftest givee-not-repeat-test
   (is (= false
