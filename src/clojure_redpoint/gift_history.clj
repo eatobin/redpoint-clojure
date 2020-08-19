a14d79cb95b2584af04d973f3034d6183853ac12
Eric Tobin <eatobin@gmail.com>
2020-08-19 17:48:31 -0500

    added scala

--- src/clojure_redpoint/gift_history.clj
+++ src/clojure_redpoint/gift_history.clj
@@ -4,6 +4,12 @@
 
 (s/def :unq/gift-history (s/coll-of :unq/gift-pair :kind vector?))
 
+object GiftHistory extends DefaultJsonProtocol {
+                                                def giftHistoryAddYear(giftHistory: GiftHistory, playerKey: PlayerKey): GiftHistory =
+                                                giftHistory :+ GiftPair(playerKey, playerKey)
 
+                                                def giftHistoryUpdateGiftHistory(giftHistory: GiftHistory, giftYear: GiftYear, giftPair: GiftPair): GiftHistory = {
+                                                                                                                                                                   giftHistory.updated(giftYear, giftPair)
+                                                                                                                                                                   }
 
 (ostest/instrument)
