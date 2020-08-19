2a9e0992c7d63c84253a5dc2efb191c90b1f9267
Eric Tobin <eatobin@gmail.com>
2020-08-19 11:58:23 -0500

    clean up name

--- src/clojure_redpoint/gift_pair.clj
+++ src/clojure_redpoint/gift_pair.clj
@@ -6,14 +6,14 @@
 (s/def ::giver keyword?)
 (s/def :unq/gift-pair (s/keys :req-un [::givee ::giver]))
 
-(defrecord GP [givee giver])
-(s/fdef ->GP
+(defrecord Gift-Pair [givee giver])
+(s/fdef ->Gift-Pair
         :args (s/cat :givee ::givee
                      :giver ::giver)
         :ret :unq/gift-pair)
 
-(def gp (GP. :me :you))
-(def gp2 (->GP :now :then))
+(def gp (Gift-Pair. :me :you))
+(def gp2 (->Gift-Pair :now :then))
 
 (s/conform :unq/gift-pair
            gp)
