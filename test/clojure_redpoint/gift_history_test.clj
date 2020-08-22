7a3d486441064a7efa230389c62e15b1deca2329
Eric Tobin <eatobin@gmail.com>
2020-08-21 21:01:17 -0500

    add-year

--- test/clojure_redpoint/gift_history_test.clj
+++ test/clojure_redpoint/gift_history_test.clj
@@ -1,15 +1,16 @@
 (ns clojure-redpoint.gift-history-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.test :refer [deftest is]]
+            [clojure-redpoint.gift-pair :as gp]
             [clojure-redpoint.gift-history :as gh]
             [clojure.spec.alpha :as s]))
 
-(def gift-history [{:giver :JohLen, :givee :GeoHar}])
+(def gift-history [(gp/->Gift-Pair :GeoHar :JohLen)])
 
-;; (deftest add-year-test
-;;   (is (= [{:giver :JohLen, :givee :GeoHar}, {:giver :NewBee, :givee :NewBee}]
-;;          (gh/add-year gift-history :NewBee))))
-;; (s/conform :unq/gift-history
-;;            (gh/add-year gift-history :NewBee))
+(deftest add-year-test
+  (is (= [(gp/->Gift-Pair :GeoHar :JohLen), (gp/->Gift-Pair :NewBee :NewBee)]
+         (gh/add-year gift-history :NewBee))))
+(s/conform :unq/gift-history
+           (gh/add-year gift-history :NewBee))
 
 ;; (deftest get-gift-pair-test
 ;;   (is (= {:giver :JohLen, :givee :GeoHar}
