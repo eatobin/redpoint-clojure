c17e7c93705faa71e3ef84ae5829109ef3e46ff7
Eric Tobin <eatobin@gmail.com>
2020-08-21 20:26:16 -0500

    progress

--- test/clojure_redpoint/gift_history_test.clj
+++ test/clojure_redpoint/gift_history_test.clj
@@ -5,20 +5,20 @@
 
 (def gift-history [{:giver :JohLen, :givee :GeoHar}])
 
-(deftest add-year-test
-  (is (= [{:giver :JohLen, :givee :GeoHar}, {:giver :NewBee, :givee :NewBee}]
-         (gh/add-year gift-history :NewBee))))
-(s/conform :unq/gift-history
-           (gh/add-year gift-history :NewBee))
+;; (deftest add-year-test
+;;   (is (= [{:giver :JohLen, :givee :GeoHar}, {:giver :NewBee, :givee :NewBee}]
+;;          (gh/add-year gift-history :NewBee))))
+;; (s/conform :unq/gift-history
+;;            (gh/add-year gift-history :NewBee))
 
-(deftest get-gift-pair-test
-  (is (= {:giver :JohLen, :givee :GeoHar}
-         (gh/get-gift-pair gift-history 0))))
-(s/conform :unq/gift-pair
-           (gh/get-gift-pair gift-history 0))
+;; (deftest get-gift-pair-test
+;;   (is (= {:giver :JohLen, :givee :GeoHar}
+;;          (gh/get-gift-pair gift-history 0))))
+;; (s/conform :unq/gift-pair
+;;            (gh/get-gift-pair gift-history 0))
 
-(deftest set-gift-pair-test
-  (is (= [{:givee :me, :giver :you}]
-         (gh/set-gift-pair gift-history 0 {:givee :me, :giver :you}))))
-(s/conform :unq/gift-history
-           (gh/set-gift-pair gift-history 0 {:givee :me, :giver :you}))
+;; (deftest set-gift-pair-test
+;;   (is (= [{:givee :me, :giver :you}]
+;;          (gh/set-gift-pair gift-history 0 {:givee :me, :giver :you}))))
+;; (s/conform :unq/gift-history
+;;            (gh/set-gift-pair gift-history 0 {:givee :me, :giver :you}))
