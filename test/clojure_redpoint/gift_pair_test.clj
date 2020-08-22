b2143894d17c68f5bc8b6164a515a3c6255f4dcc
Eric Tobin <eatobin@gmail.com>
2020-08-21 20:06:29 -0500

    gift-pair tests

--- test/clojure_redpoint/gift_pair_test.clj
+++ test/clojure_redpoint/gift_pair_test.clj
@@ -1,5 +1,5 @@
 (ns clojure-redpoint.gift-pair-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.test :refer [deftest is]]
             [clojure-redpoint.gift-pair :as gp]
             [clojure.spec.alpha :as s]))
 
@@ -10,17 +10,17 @@
          (:givee gift-pair)))
   (is (= :JohLen
          (:giver gift-pair))))
-;(s/conform ::gp/givee
-;           (gp/get-givee gift-pair))
-;(s/conform ::gp/giver
-;           (gp/get-giver gift-pair))
-;
-;(deftest set-giv-ee-er-test
-;  (is (= {:giver :JohLen, :givee :NewBee}
-;         (gp/set-givee gift-pair :NewBee)))
-;  (is (= {:givee :GeoHar, :giver :NewBee}
-;         (gp/set-giver gift-pair :NewBee))))
-;(s/conform :unq/gift-pair
-;           (gp/set-givee gift-pair :NewBee))
-;(s/conform :unq/gift-pair
-;           (gp/set-giver gift-pair :NewBee))
+(s/conform ::gp/givee
+           (:givee gift-pair))
+(s/conform ::gp/giver
+           (:giver gift-pair))
+
+(deftest set-giv-ee-er-test
+  (is (= (gp/->Gift-Pair :NewBee :JohLen)
+         (gp/set-givee gift-pair :NewBee)))
+  (is (= (gp/->Gift-Pair :GeoHar :NewBee)
+         (gp/set-giver gift-pair :NewBee))))
+(s/conform :unq/gift-pair
+           (gp/set-givee gift-pair :NewBee))
+(s/conform :unq/gift-pair
+           (gp/set-giver gift-pair :NewBee))
