7ff4f1f89129df54d184532cee1a90e467a8df4d
Eric Tobin <eatobin@gmail.com>
2020-08-28 18:03:25 -0500

    hats progress

--- src/clojure_redpoint/hats.clj
+++ src/clojure_redpoint/hats.clj
@@ -1,28 +1,30 @@
 (ns clojure-redpoint.hats
-  (:require [clojure-redpoint.roster :as ros]
+  (:require [clojure-redpoint.gift-pair :as gp]
+            [clojure-redpoint.players :as plrs]
             [clojure.spec.alpha :as s]
             [orchestra.spec.test :as ostest]))
 
-(s/def ::hat (s/coll-of ::ros/player-key :kind set?))
-(s/def ::discards (s/coll-of ::ros/player-key :kind set?))
+(s/def ::hat (s/coll-of ::plrs/player-key :kind set?))
+(s/def ::discards (s/coll-of ::plrs/player-key :kind set?))
 
 (defn make-hat [players]
   (into #{} (keys players)))
 (s/fdef make-hat
-        :args (s/cat :players :unq/players)
+        :args (s/cat :players ::plrs/players)
         :ret ::hat)
 
 (defn remove-puck [hat plr-key]
   (into #{} (remove #(= % plr-key) hat)))
 (s/fdef remove-puck
         :args (s/cat :hat ::hat
-                     :plr-key ::ros/player-key)
+                     :plr-key ::plrs/player-key)
         :ret ::hat)
 
 (defn discard-givee [discards givee]
   (conj discards givee))
 (s/fdef discard-givee
-        :args (s/cat :discards ::discards :givee ::ros/givee)
+        :args (s/cat :discards ::discards
+                     :givee ::gp/givee)
         :ret ::discards)
 
 (defn return-discards [ge-hat discards]
