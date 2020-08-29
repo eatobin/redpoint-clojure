bf23131857f97f9ccec68c5d79bcee2c6bf05eeb
Eric Tobin <eatobin@gmail.com>
2020-08-29 09:50:38 -0500

    roster_test.clj progress

--- test/clojure_redpoint/roster_test.clj
+++ test/clojure_redpoint/roster_test.clj
@@ -1,5 +1,6 @@
 (ns clojure-redpoint.roster-test
-  (:require [clojure-redpoint.player :as plr]
+  (:require [clojure-redpoint.gift-pair :as gp]
+            [clojure-redpoint.player :as plr]
             [clojure.test :refer [deftest is]]
             [clojure-redpoint.roster :as ros]
             [clojure.spec.alpha :as s]))
@@ -7,11 +8,11 @@
 (def roster {:roster-name "The Beatles",
              :roster-year 2014,
              :players     {:PauMcc (plr/map->Player {:player-name  "Paul McCartney",
-                                                     :gift-history [{:giver :JohLen, :givee :GeoHar}]}),
+                                                     :gift-history [(gp/map->Gift-Pair {:giver :JohLen, :givee :GeoHar})]}),
                            :GeoHar (plr/map->Player {:player-name  "George Harrison",
-                                                     :gift-history [{:giver :PauMcc, :givee :RinSta}]}),
-                           :JohLen (plr/map->Player {:player-name "John Lennon", :gift-history [{:giver :RinSta, :givee :PauMcc}]}),
-                           :RinSta (plr/map->Player {:player-name "Ringo Starr", :gift-history [{:giver :GeoHar, :givee :JohLen}]})}})
+                                                     :gift-history [(gp/map->Gift-Pair {:giver :PauMcc, :givee :RinSta})]}),
+                           :JohLen (plr/map->Player {:player-name "John Lennon", :gift-history [(gp/map->Gift-Pair {:giver :RinSta, :givee :PauMcc})]}),
+                           :RinSta (plr/map->Player {:player-name "Ringo Starr", :gift-history [(gp/map->Gift-Pair {:giver :GeoHar, :givee :JohLen})]})}})
 
 (s/conform ::ros/roster
            roster)
