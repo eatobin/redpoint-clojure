4037f1f76cff52226a0aa0b337feefeb48d3ed45
Eric Tobin <eatobin@gmail.com>
2020-08-29 20:21:21 -0500

    rules progress

--- src/clojure_redpoint/rules.clj
+++ src/clojure_redpoint/rules.clj
@@ -1,5 +1,7 @@
 (ns clojure-redpoint.rules
-  (:require [clojure-redpoint.roster :as ros]
+  (:require [clojure-redpoint.gift-pair :as gp]
+            [clojure-redpoint.players :as plrs]
+            [clojure-redpoint.gift-history :as gh]
             [clojure.spec.alpha :as s]
             [orchestra.spec.test :as ostest]))
 
@@ -8,35 +10,35 @@
   [self-key givee]
   (not= self-key givee))
 (s/fdef givee-not-self?
-  :args (s/cat :self-key ::ros/player-key
-               :givee ::ros/givee)
-  :ret boolean?)
+        :args (s/cat :self-key ::plrs/player-key
+                     :givee ::gp/givee)
+        :ret boolean?)
 
 (defn givee-not-recip?
   "Test 2 - not giving to the person who is giving to you"
   [self-key givee g-year players]
-  (let [recip (ros/get-givee players givee g-year)]
+  (let [recip (plrs/players-get-givee players givee g-year)]
     (not= self-key recip)))
 (s/fdef givee-not-recip?
-  :args (s/cat :self-key ::ros/player-key
-               :givee ::ros/givee
-               :g-year ::ros/gift-year
-               :players :unq/players)
-  :ret boolean?)
+        :args (s/cat :self-key ::plrs/player-key
+                     :givee ::gp/givee
+                     :g-year ::gh/gift-year
+                     :players ::plrs/players)
+        :ret boolean?)
 
 (defn givee-not-repeat?
   "Test 3 - not giving to someone you have given to in the past 3 years"
   [self-key givee g-year players]
   (let [past (filter #(>= % 0)
                      (range (- g-year 1) (- g-year 4) -1))
-        ge-y (partial ros/get-givee players self-key)
+        ge-y (partial plrs/players-get-givee players self-key)
         ge-in-yrs (into [] (map ge-y past))]
     (not-any? #{givee} ge-in-yrs)))
 (s/fdef givee-not-repeat?
-  :args (s/cat :self-key ::ros/player-key
-               :givee ::ros/givee
-               :g-year ::ros/gift-year
-               :players :unq/players)
-  :ret boolean?)
+        :args (s/cat :self-key ::plrs/player-key
+                     :givee ::gp/givee
+                     :g-year :gh/gift-year
+                     :players ::plrs/players)
+        :ret boolean?)
 
 (ostest/instrument)
