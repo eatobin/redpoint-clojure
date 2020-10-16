5015b9d1ef56ea6950b414d92def8edb6f5aa43f
Eric Tobin <eatobin@gmail.com>
2020-10-15 21:13:38 -0500

    fix skipped merge

--- src/redpoint/rules.clj
+++ src/redpoint/rules.clj
@@ -1,5 +1,6 @@
 (ns redpoint.rules
-  (:require [redpoint.roster :as ros]
+  (:require [redpoint.domain :as dom]
+            [redpoint.players :as plrs]
             [clojure.spec.alpha :as s]
             [orchestra.spec.test :as ostest]))
 
@@ -8,35 +9,35 @@
   [self-key givee]
   (not= self-key givee))
 (s/fdef givee-not-self?
-  :args (s/cat :self-key ::ros/player-key
-               :givee ::ros/givee)
-  :ret boolean?)
+        :args (s/cat :self-key ::dom/player-key
+                     :givee ::dom/givee)
+        :ret boolean?)
 
 (defn givee-not-recip?
   "Test 2 - not giving to the person who is giving to you"
   [self-key givee g-year players]
-  (let [recip (ros/get-givee players givee g-year)]
+  (let [recip (plrs/players-get-givee players givee g-year)]
     (not= self-key recip)))
 (s/fdef givee-not-recip?
-  :args (s/cat :self-key ::ros/player-key
-               :givee ::ros/givee
-               :g-year ::ros/gift-year
-               :players :unq/players)
-  :ret boolean?)
+        :args (s/cat :self-key ::dom/player-key
+                     :givee ::dom/givee
+                     :g-year ::dom/gift-year
+                     :players ::dom/players)
+        :ret boolean?)
 
 (defn givee-not-repeat?
   "Test 3 - not giving to someone you have given to in the past 3 years"
   [self-key givee g-year players]
   (let [past (filter #(>= % 0)
                      (range (- g-year 1) (- g-year 4) -1))
-        ge-y (partial ros/get-givee players self-key)
+        ge-y (partial plrs/players-get-givee players self-key)
         ge-in-yrs (into [] (map ge-y past))]
     (not-any? #{givee} ge-in-yrs)))
 (s/fdef givee-not-repeat?
-  :args (s/cat :self-key ::ros/player-key
-               :givee ::ros/givee
-               :g-year ::ros/gift-year
-               :players :unq/players)
-  :ret boolean?)
+        :args (s/cat :self-key ::dom/player-key
+                     :givee ::dom/givee
+                     :g-year ::dom/gift-year
+                     :players ::dom/players)
+        :ret boolean?)
 
 (ostest/instrument)
