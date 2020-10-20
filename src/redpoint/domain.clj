1e942db61a0391e97bf50f1efec00a8e9aff9bf6
Eric Tobin <eatobin@gmail.com>
2020-10-20 18:03:46 -0500

    simplify progress

--- src/redpoint/domain.clj
+++ src/redpoint/domain.clj
@@ -1,22 +1,22 @@
 (ns redpoint.domain
   (:require [clojure.spec.alpha :as s]))
 
-(s/def ::givee keyword?)
-(s/def ::giver keyword?)
-(s/def ::gift-pair (s/keys :req-un [::givee ::giver]))
+(s/def ::givee-str keyword?)
+(s/def ::giver-str keyword?)
+(s/def :unq/gift-pair (s/keys :req-un [::givee ::giver]))
 
-(s/def ::gift-history (s/coll-of ::gift-pair :kind vector?))
+(s/def :unq/gift-history (s/coll-of :unq/gift-pair :kind vector?))
 (s/def ::player-key keyword?)
 (s/def ::gift-year (s/and int? #(> % -1)))
 
 (s/def ::player-name string?)
-(s/def ::player (s/keys :req-un [::player-name ::gift-history]))
+(s/def :unq/player (s/keys :req-un [::player-name :unq/gift-history]))
 
-(s/def ::players (s/map-of ::player-key ::player))
+(s/def :unq/players (s/map-of ::player-key :unq/player))
 
 (s/def ::roster-name string?)
 (s/def ::roster-year int?)
-(s/def ::roster (s/keys :req-un [::roster-name ::roster-year ::players]))
+(s/def :unq/roster (s/keys :req-un [::roster-name ::roster-year :unq/players]))
 
-(s/def ::hat (s/coll-of ::player-key :kind set?))
-(s/def ::discards (s/coll-of ::player-key :kind set?))
+(s/def :unq/hat (s/coll-of ::player-key :kind set?))
+(s/def :unq/discards (s/coll-of ::player-key :kind set?))
--- src/redpoint/gift_pair.clj
+++ src/redpoint/gift_pair.clj
@@ -4,41 +4,43 @@
             [clojure.spec.alpha :as s]
             [orchestra.spec.test :as ostest]))
 
-(defrecord Gift-Pair [givee giver])
-(s/fdef ->Gift-Pair
-        :args (s/cat :givee ::dom/givee
-                     :giver ::dom/giver)
-        :ret ::dom/gift-pair)
+;(defrecord Gift-Pair [givee giver])
+;(defn make-gift-pair [givee-str giver-str]
+;  {:givee givee-str :giver giver-str})
+;(s/fdef make-gift-pair
+;        :args (s/cat :givee-str ::dom/givee
+;                     :givee-str ::dom/giver)
+;        :ret :unq/gift-pair)
 
-(defn gift-pair-update-givee [gift-pair givee]
-  (assoc gift-pair :givee givee))
-(s/fdef gift-pair-update-givee
-        :args (s/cat :gift-pair ::dom/gift-pair
-                     :givee ::dom/givee)
-        :ret ::dom/gift-pair)
-
-(defn gift-pair-update-giver [gift-pair giver]
-  (assoc gift-pair :giver giver))
-(s/fdef gift-pair-update-giver
-        :args (s/cat :gift-pair ::dom/gift-pair
-                     :giver ::dom/giver)
-        :ret ::dom/gift-pair)
-
-(defn- my-value-reader
-  [key value]
-  (if (or (= key :givee)
-          (= key :giver))
-    (keyword value)
-    value))
-
-(defn gift-pair-json-string-to-Gift-Pair [gp-string]
-  (let [gift-pair (json/read-str gp-string
-                                 :value-fn my-value-reader
-                                 :key-fn keyword)]
-    (->Gift-Pair (:givee gift-pair)
-                 (:giver gift-pair))))
-(s/fdef gift-pair-json-string-to-Gift-Pair
-        :args (s/cat :gp-string string?)
-        :ret ::dom/gift-pair)
+;(defn gift-pair-update-givee [gift-pair givee]
+;  (assoc gift-pair :givee givee))
+;(s/fdef gift-pair-update-givee
+;        :args (s/cat :gift-pair ::dom/gift-pair
+;                     :givee ::dom/givee)
+;        :ret ::dom/gift-pair)
+;
+;(defn gift-pair-update-giver [gift-pair giver]
+;  (assoc gift-pair :giver giver))
+;(s/fdef gift-pair-update-giver
+;        :args (s/cat :gift-pair ::dom/gift-pair
+;                     :giver ::dom/giver)
+;        :ret ::dom/gift-pair)
+;
+;(defn- my-value-reader
+;  [key value]
+;  (if (or (= key :givee)
+;          (= key :giver))
+;    (keyword value)
+;    value))
+;
+;(defn gift-pair-json-string-to-Gift-Pair [gp-string]
+;  (let [gift-pair (json/read-str gp-string
+;                                 :value-fn my-value-reader
+;                                 :key-fn keyword)]
+;    (->Gift-Pair (:givee gift-pair)
+;                 (:giver gift-pair))))
+;(s/fdef gift-pair-json-string-to-Gift-Pair
+;        :args (s/cat :gp-string string?)
+;        :ret ::dom/gift-pair)
 
 (ostest/instrument)
