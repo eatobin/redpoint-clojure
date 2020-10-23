2c76366bf2ef999168b16ecfac3d06b6b377f1b2
Eric Tobin <eatobin@gmail.com>
2020-10-22 22:02:55 -0500

    gift history passes!!

--- src/redpoint/gift_history.clj
+++ src/redpoint/gift_history.clj
@@ -1,30 +1,29 @@
 (ns redpoint.gift-history
   (:require [redpoint.domain :as dom]
             [clojure.data.json :as json]
-            [redpoint.gift-pair :as gp]
             [clojure.spec.alpha :as s]
             [orchestra.spec.test :as ostest]))
 
 (defn gift-history-add-year
   "Adds a new placeholder year to the end of a player's gift history"
   [g-hist plr-key]
-  (conj g-hist (gp/->Gift-Pair plr-key plr-key)))
+  (conj g-hist {:givee plr-key :giver plr-key}))
 (s/fdef gift-history-add-year
-        :args (s/cat :g-hist ::dom/gift-history
-                     :plr-key ::dom/player-key)
-        :ret ::dom/gift-history)
+  :args (s/cat :g-hist :unq/gift-history
+               :plr-key ::dom/player-key)
+  :ret :unq/gift-history)
 
 (defn gift-history-update-gift-history
   "Returns a gift history with the provided gift pair at the supplied year"
   [g-hist g-year g-pair]
   (assoc g-hist g-year g-pair))
 (s/fdef gift-history-update-gift-history
-        :args (s/and
-                (s/cat :g-hist ::dom/gift-history
-                       :g-year ::dom/gift-year
-                       :g-pair ::dom/gift-pair)
-                #(< (:g-year %) (count (:g-hist %))))
-        :ret ::dom/gift-history)
+  :args (s/and
+         (s/cat :g-hist :unq/gift-history
+                :g-year ::dom/gift-year
+                :g-pair :unq/gift-pair)
+         #(< (:g-year %) (count (:g-hist %))))
+  :ret :unq/gift-history)
 
 (defn- my-value-reader
   [key value]
@@ -34,14 +33,11 @@
     value))
 
 (defn gift-history-json-string-to-Gift-History [gh-string]
-  (let [gift-history (json/read-str gh-string
-                                    :value-fn my-value-reader
-                                    :key-fn keyword)]
-    (vec (map #(gp/->Gift-Pair (:givee %)
-                               (:giver %))
-              gift-history))))
+  (vec (json/read-str gh-string
+                      :value-fn my-value-reader
+                      :key-fn keyword)))
 (s/fdef gift-history-json-string-to-Gift-History
-        :args (s/cat :gh-string string?)
-        :ret ::dom/gift-history)
+  :args (s/cat :gh-string string?)
+  :ret :unq/gift-history)
 
 (ostest/instrument)
--- test/redpoint/gift_history_test.clj
+++ test/redpoint/gift_history_test.clj
@@ -1,30 +1,28 @@
 (ns redpoint.gift-history-test
   (:require [clojure.test :refer [deftest is]]
-            [redpoint.domain :as dom]
-            [redpoint.gift-pair :as gp]
             [redpoint.gift-history :as gh]
             [clojure.spec.alpha :as s]))
 
 (def json-string-GH "[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]")
-(def gift-history [(gp/->Gift-Pair :GeoHar :JohLen)])
+(def gift-history [{:givee :GeoHar :giver :JohLen}])
 
 (deftest add-year-test
-  (is (= [(gp/->Gift-Pair :GeoHar :JohLen), (gp/->Gift-Pair :NewBee :NewBee)]
+  (is (= [{:givee :GeoHar :giver :JohLen}, {:givee :NewBee :giver :NewBee}]
          (gh/gift-history-add-year gift-history :NewBee))))
-(s/conform ::dom/gift-history
+(s/conform :unq/gift-history
            (gh/gift-history-add-year gift-history :NewBee))
 
 (deftest get-gift-pair-test
-  (is (= (gp/map->Gift-Pair {:giver :JohLen, :givee :GeoHar})
+  (is (= {:givee :GeoHar :giver :JohLen}
          (gift-history 0))))
-(s/conform ::dom/gift-pair
+(s/conform :unq/gift-pair
            (gift-history 0))
 
 (deftest set-gift-pair-test
-  (is (= [(gp/->Gift-Pair :me :you)]
-         (gh/gift-history-update-gift-history gift-history 0 (gp/->Gift-Pair :me :you)))))
-(s/conform ::dom/gift-history
-           (gh/gift-history-update-gift-history gift-history 0 (gp/map->Gift-Pair {:givee :me, :giver :you})))
+  (is (= [{:givee :me :giver :you}]
+         (gh/gift-history-update-gift-history gift-history 0 {:givee :me :giver :you}))))
+(s/conform :unq/gift-history
+           (gh/gift-history-update-gift-history gift-history 0 {:givee :me :giver :you}))
 
 (deftest gift-history-json-string-to-Gift-History-test
   (is (= (gh/gift-history-json-string-to-Gift-History json-string-GH)
