d52b0f15a0471e5be28487efa1b3a7246fa34bcd
Eric Tobin <eatobin@gmail.com>
2020-10-24 09:52:03 -0500

    player tests!!!

--- src/redpoint/player.clj
+++ src/redpoint/player.clj
@@ -1,7 +1,5 @@
 (ns redpoint.player
-  (:require [redpoint.domain :as dom]
-            [redpoint.gift-pair :as gp]
-            [clojure.data.json :as json]
+  (:require [clojure.data.json :as json]
             [clojure.spec.alpha :as s]
             [orchestra.spec.test :as ostest]))
 
@@ -10,29 +8,30 @@
   [player g-hist]
   (assoc player :gift-history g-hist))
 (s/fdef player-update-gift-history
-  :args (s/cat :player :unq/player
-               :g-hist :unq/gift-history)
-  :ret :unq/player)
+        :args (s/cat :player :unq/player
+                     :g-hist :unq/gift-history)
+        :ret :unq/player)
 
-;; (defn- my-key-reader
-;;   [key]
-;;   (cond
-;;     (= key "playerName") :player-name
-;;     (= key "giftHistory") :gift-history
-;;     :else (keyword key)))
+(defn- my-key-reader
+  [key]
+  (cond
+    (= key "playerName") :player-name
+    (= key "giftHistory") :gift-history
+    :else (keyword key)))
 
-;; (defn- my-value-reader
-;;   [key value]
-;;   (if (or (= key :givee)
-;;           (= key :giver))
-;;     (keyword value)
-;;     value))
+(defn- my-value-reader
+  [key value]
+  (if (or (= key :givee)
+          (= key :giver))
+    (keyword value)
+    value))
 
 (defn player-json-string-to-Player [plr-string]
   (json/read-str plr-string
-                 :key-fn keyword))
+                 :value-fn my-value-reader
+                 :key-fn my-key-reader))
 (s/fdef player-json-string-to-Player
-  :args (s/cat :plr-string string?)
-  :ret :unq/player)
+        :args (s/cat :plr-string string?)
+        :ret :unq/player)
 
 (ostest/instrument)
--- test/redpoint/player_test.clj
+++ test/redpoint/player_test.clj
@@ -1,15 +1,11 @@
 (ns redpoint.player-test
   (:require [clojure.test :refer [deftest is]]
-            [redpoint.domain :as dom]
-            [redpoint.gift-pair :as gp]
             [redpoint.player :as plr]
             [clojure.spec.alpha :as s]))
 
 (def json-string-Player "{\"playerName\":\"Paul McCartney\",\"giftHistory\":[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]}")
 (def player {:player-name  "Paul McCartney",
              :gift-history [{:givee :GeoHar :giver :JohLen}]})
-;; (def player (plr/map->Player {:player-name  "Paul McCartney",
-;;                               :gift-history [(gp/->Gift-Pair :GeoHar :JohLen)]}))
 
 (deftest player-update-gift-history-test
   (is (= {:player-name  "Paul McCartney",
@@ -18,12 +14,8 @@
 (s/conform :unq/player
            (plr/player-update-gift-history player [{:givee :nope :giver :yup}]))
 
-;; (deftest player-plain-upgrade-test
-;;   (is (= player
-;;          (plr/player-plain-upgrade plain-player))))
-;; (s/conform ::dom/player
-;;            (plr/player-plain-upgrade plain-player))
-
 (deftest player-json-string-to-Player-test
   (is (= (plr/player-json-string-to-Player json-string-Player)
          player)))
+(s/conform :unq/player
+           (plr/player-json-string-to-Player json-string-Player))
