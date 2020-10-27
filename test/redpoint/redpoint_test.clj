dbebb102b70fccacb9b3b47c3801c591d7da9507
Eric Tobin <eatobin@gmail.com>
2020-10-26 19:55:03 -0500

    all test!!!!!

--- test/redpoint/redpoint_test.clj
+++ test/redpoint/redpoint_test.clj
@@ -2,8 +2,6 @@
   (:require [clojure.test :refer [deftest is]]
             [redpoint.domain :as dom]
             [redpoint.redpoint :as core]
-            [redpoint.gift-pair :as gp]
-            [redpoint.player :as plr]
             [redpoint.players :as plrs]
             [redpoint.hats :as hat]
             [clojure.spec.alpha :as s]))
@@ -38,81 +36,81 @@
              (deref core/a-players)))
 
 (deftest draw-puck-test
- (is (true?
+  (is (true?
        (nil? (core/draw-puck #{}))))
- (is (true?
+  (is (true?
        (some? (core/draw-puck test-hat)))))
 (s/conform ::dom/givee
-          (core/draw-puck test-hat))
+           (core/draw-puck test-hat))
 (s/conform nil?
-          (core/draw-puck #{}))
+           (core/draw-puck #{}))
 
-;(deftest start-new-year-test
-;  (reset! core/a-g-year 0)
-;  (reset! core/a-giver nil)
-;  (reset! core/a-givee nil)
-;  (core/roster-or-quit "resources-test/beatles.json")
-;  (core/start-new-year)
-;  (is (= 1
-;         (deref core/a-g-year)))
-;  (is (some?
-;        (deref core/a-giver)))
-;  (is (some?
-;        (deref core/a-givee)))
-;  (is (= (plr/map->Player {:player-name  "Ringo Starr",
-;                           :gift-history [(gp/map->Gift-Pair {:givee :JohLen, :giver :GeoHar})
-;                                          (gp/map->Gift-Pair {:givee :RinSta, :giver :RinSta})]})
-;         (get-in (deref core/a-players) [:RinSta])))
-;  (is (empty? (deref core/a-discards))))
-;
-;(deftest select-new-giver-test
-;  (reset! core/a-g-year 0)
-;  (reset! core/a-giver nil)
-;  (reset! core/a-givee nil)
-;  (core/roster-or-quit "resources-test/beatles.json")
-;  (core/start-new-year)
-;  (swap! core/a-discards hat/discard-givee :GeoHar)
-;  (is (= 1
-;         (count (deref core/a-discards))))
-;  (core/select-new-giver)
-;  (is (= 3
-;         (count (deref core/a-gr-hat))))
-;  (is (= 0
-;         (count (deref core/a-discards)))))
-;
-;(deftest givee-is-success-test
-;  (reset! core/a-g-year 0)
-;  (reset! core/a-giver nil)
-;  (reset! core/a-givee nil)
-;  (core/roster-or-quit "resources-test/beatles.json")
-;  (core/start-new-year)
-;  (let [temp-ge (deref core/a-givee)]
-;    (core/givee-is-success)
-;    (is (= temp-ge
-;           (plrs/players-get-givee (deref core/a-players) (deref core/a-giver) (deref core/a-g-year))))
-;    (is (= (deref core/a-giver)
-;           (plrs/players-get-giver (deref core/a-players) temp-ge (deref core/a-g-year))))
-;    (is (= nil
-;           (some #{temp-ge} (deref core/a-ge-hat))))))
-;
-;(deftest givee-is-failure-test
-;  (reset! core/a-g-year 0)
-;  (reset! core/a-giver nil)
-;  (reset! core/a-givee nil)
-;  (core/roster-or-quit "resources-test/beatles.json")
-;  (core/start-new-year)
-;  (let [temp-ge (deref core/a-givee)]
-;    (core/givee-is-failure)
-;    (is (= temp-ge
-;           (some #{temp-ge} (deref core/a-discards))))
-;    (is (= nil
-;           (some #{temp-ge} (deref core/a-ge-hat))))))
-;
-;(deftest errors?-test
-;  (reset! core/a-g-year 0)
-;  (reset! core/a-players {:RinSta (plr/map->Player {:player-name "Ringo Starr", :gift-history [(gp/map->Gift-Pair {:givee :JohLen, :giver :GeoHar})]}),
-;                          :JohLen (plr/map->Player {:player-name "John Lennon", :gift-history [(gp/map->Gift-Pair {:givee :PauMcc, :giver :RinSta})]}),
-;                          :GeoHar (plr/map->Player {:player-name "George Harrison", :gift-history [(gp/map->Gift-Pair {:givee :GeoHar, :giver :PauMcc})]}),
-;                          :PauMcc (plr/map->Player {:player-name "Paul McCartney", :gift-history [(gp/map->Gift-Pair {:givee :GeoHar, :giver :PauMcc})]})})
-;  (is (= (seq [:GeoHar :PauMcc])
-;         (core/errors?))))
+(deftest start-new-year-test
+  (reset! core/a-g-year 0)
+  (reset! core/a-giver nil)
+  (reset! core/a-givee nil)
+  (core/roster-or-quit "resources-test/beatles.json")
+  (core/start-new-year)
+  (is (= 1
+         (deref core/a-g-year)))
+  (is (some?
+       (deref core/a-giver)))
+  (is (some?
+       (deref core/a-givee)))
+  (is (= {:player-name  "Ringo Starr",
+          :gift-history [{:givee :JohLen, :giver :GeoHar}
+                         {:givee :RinSta, :giver :RinSta}]}
+         (get-in (deref core/a-players) [:RinSta])))
+  (is (empty? (deref core/a-discards))))
+
+(deftest select-new-giver-test
+  (reset! core/a-g-year 0)
+  (reset! core/a-giver nil)
+  (reset! core/a-givee nil)
+  (core/roster-or-quit "resources-test/beatles.json")
+  (core/start-new-year)
+  (swap! core/a-discards hat/discard-givee :GeoHar)
+  (is (= 1
+         (count (deref core/a-discards))))
+  (core/select-new-giver)
+  (is (= 3
+         (count (deref core/a-gr-hat))))
+  (is (= 0
+         (count (deref core/a-discards)))))
+
+(deftest givee-is-success-test
+  (reset! core/a-g-year 0)
+  (reset! core/a-giver nil)
+  (reset! core/a-givee nil)
+  (core/roster-or-quit "resources-test/beatles.json")
+  (core/start-new-year)
+  (let [temp-ge (deref core/a-givee)]
+    (core/givee-is-success)
+    (is (= temp-ge
+           (plrs/players-get-givee (deref core/a-players) (deref core/a-giver) (deref core/a-g-year))))
+    (is (= (deref core/a-giver)
+           (plrs/players-get-giver (deref core/a-players) temp-ge (deref core/a-g-year))))
+    (is (= nil
+           (some #{temp-ge} (deref core/a-ge-hat))))))
+
+(deftest givee-is-failure-test
+  (reset! core/a-g-year 0)
+  (reset! core/a-giver nil)
+  (reset! core/a-givee nil)
+  (core/roster-or-quit "resources-test/beatles.json")
+  (core/start-new-year)
+  (let [temp-ge (deref core/a-givee)]
+    (core/givee-is-failure)
+    (is (= temp-ge
+           (some #{temp-ge} (deref core/a-discards))))
+    (is (= nil
+           (some #{temp-ge} (deref core/a-ge-hat))))))
+
+(deftest errors?-test
+  (reset! core/a-g-year 0)
+  (reset! core/a-players {:RinSta {:player-name "Ringo Starr", :gift-history [{:givee :JohLen, :giver :GeoHar}]},
+                          :JohLen {:player-name "John Lennon", :gift-history [{:givee :PauMcc, :giver :RinSta}]},
+                          :GeoHar {:player-name "George Harrison", :gift-history [{:givee :GeoHar, :giver :PauMcc}]},
+                          :PauMcc {:player-name "Paul McCartney", :gift-history [{:givee :GeoHar, :giver :PauMcc}]}})
+  (is (= (seq [:GeoHar :PauMcc])
+         (core/errors?))))
