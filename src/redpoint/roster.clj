a2b4e6221b87b22d52395ea2a8e3bcdd7526ae25
Eric Tobin <eatobin@gmail.com>
2020-11-12 16:29:09 -0600

    format

--- src/redpoint/roster.clj
+++ src/redpoint/roster.clj
@@ -20,7 +20,8 @@
     (keyword value)
     value))
 
-(defn roster-json-string-to-Roster [[error-string json-string]]
+(defn roster-json-string-to-Roster
+  [[error-string json-string]]
   (if (nil? error-string)
     (try
       [nil (json/read-str json-string
