5fa627a73bb920a5889e6a5151e11ae983b3de91
Eric Tobin <eatobin@gmail.com>
2023-04-28 11:30:42 -0500

    big redo

--- test/redpoint/gift_pair_test.clj
+++ test/redpoint/gift_pair_test.clj
@@ -1,7 +1,7 @@
 (ns redpoint.gift-pair-test
   (:require [clojure.spec.alpha :as s]
-            [clojure.test :as t]
-            [redpoint.gift-pair :as gp]))
+    [clojure.test :as t]
+    [redpoint.gift-pair :as gp]))
 
 (def json-string "{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}")
 (def bad-json-string "{\"givee\"\"GeoHar\",\"giver\":\"JohLen\"}")
@@ -12,26 +12,26 @@
 
 (t/deftest gift-pair-json-string-to-Gift-Pair-test
   (t/is (= gift-pair
-           (gp/gift-pair-json-string-to-Gift-Pair json-string)))
+          (gp/gift-pair-json-string-to-Gift-Pair json-string)))
   (t/is (thrown? Exception
-                 (gp/gift-pair-json-string-to-Gift-Pair bad-json-string)))
+          (gp/gift-pair-json-string-to-Gift-Pair bad-json-string)))
   (t/is (thrown? Exception
-                 (gp/gift-pair-json-string-to-Gift-Pair bad-json-string-2))))
+          (gp/gift-pair-json-string-to-Gift-Pair bad-json-string-2))))
 
 (s/conform ::gp/givee
-           (:givee gift-pair))
+  (:givee gift-pair))
 (s/conform ::gp/giver
-           (:giver gift-pair))
+  (:giver gift-pair))
 
 (t/deftest gift-pair-update-givee-test
   (t/is (= {:givee :NewBee :giver :JohLen}
-           (gp/gift-pair-update-givee :NewBee gift-pair))))
+          (gp/gift-pair-update-givee :NewBee gift-pair))))
 
 (t/deftest gift-pair-update-giver-test
   (t/is (= {:givee :GeoHar :giver :NewBee}
-           (gp/gift-pair-update-giver :NewBee gift-pair))))
+          (gp/gift-pair-update-giver :NewBee gift-pair))))
 
 (s/conform :unq/gift-pair
-           (gp/gift-pair-update-givee :NewBee gift-pair))
+  (gp/gift-pair-update-givee :NewBee gift-pair))
 (s/conform :unq/gift-pair
-           (gp/gift-pair-update-giver :NewBee gift-pair))
+  (gp/gift-pair-update-giver :NewBee gift-pair))
