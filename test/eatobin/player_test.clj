3af3048c0a9fea9adf5a4719079d3439d55923bc
Eric Tobin <eatobin@gmail.com>
2024-05-27 21:45:49 -0700

    format

--- test/eatobin/player_test.clj
+++ test/eatobin/player_test.clj
@@ -1,8 +1,8 @@
 (ns eatobin.player-test
   (:require [clojure.spec.alpha :as s]
             [clojure.test :refer [deftest is testing]]
-            [eatobin.player :refer [player-update-gift-history
-                                    player-json-string-to-player]]))
+            [eatobin.player :refer [player-json-string-to-player
+                                    player-update-gift-history]]))
 
 (def player-test-json-string "{\"playerName\":\"Paul McCartney\",\"giftHistory\":[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]}")
 (def player {:player-name "Paul McCartney" :gift-history [{:givee :GeoHar :giver :JohLen}]})
